fb478edd3afd34f46cc577111d2611df96e361e0
Matthew Bold <boldm1@ega091000004.lancs.ac.uk>
2019-05-03 12:33:15 +0100

    Changing load_instance.py to import frcpsp/max instances

--- load_instance.py
+++ load_instance.py
@@ -1,4 +1,6 @@
 import re
+from pathlib import Path
+
 from task import Task
 from project import Project
 from temporal_analysis import temporal_analysis
@@ -11,35 +13,42 @@ def load_instance(path_to_file):
         line = line.replace('\t', ',').replace('[', '').replace(']', '')
         stripped_lines.append(re.split(',', line))
     first_line = stripped_lines[0]
-    n_activities = int(first_line[0]) + 2 # number of activities (incl. dummy activities)
+    n_activities = int(first_line[0]) # number of activities (incl. dummy activities)
     n_resources = int(first_line[1])
     ### loading tasks ###
     tasks = {}
     for activity in range(n_activities):
         ### first block ###
         line1 = stripped_lines[activity+1]
-        line2 = stripped_lines[n_activities+activity+1]
         task_id = int(line1[0])
-        n_successors = int(line1[2])
-        successors = []
-        if n_successors > 0:
-            for i in range(n_successors):
-                successors.append((int(line1[3+i]), int(line1[3+n_successors+i]))) # ((F-->S) successor_id, min. time-lag)
+        n_successors = [int(line1[2]), int(line1[3]), int(line1[4]), int(line1[5])] # [# SS successors, # SF successors, # FS successors, # FF successors]
+        successors = [[] for i in range(4)]
+        k = 0 # counter to track where in line1 to get desired info.
+        for i in range(4):
+            if n_successors[i] > 0:
+                for j in range(n_successors[i]):
+                    successors[i].append((int(line1[6+2*k+j]), int(line1[6+2*k+n_successors[i]+j]))) # e.g. successor[i=2(FS)] = [(FS successor id, min. time-lag),...]
+            k += n_successors[i]
         ### second block ###
-        d = int(line2[2]) # activity duration
-        q = [] # quantity of resource required of each resource
+        line2 = stripped_lines[n_activities+activity+1]
+        w = int(line2[3]) # work content
+        q_min = [] # min. per-period resource allocation for each resource 
+        q_max = [] # max. per-period resource allocation for each resource
         for r in range(n_resources):
-            q.append(int(line2[3+r])) 
-        task = Task(task_id, successors, d, q)
+            q_min.append(int(line2[4+2*r]))
+            q_max.append(int(line2[4+2*r+1]))
+        task = Task(task_id, successors, w, q_min, q_max)
         tasks[task_id] = task 
     last_line = stripped_lines[2*n_activities+1]
     R = [] # resource_availabilities
     for r in range(n_resources):
         R.append(int(last_line[r]))
-    project = Project(path_to_file, tasks, R)
+    l = int(last_line[n_resources]) # min. block length
+    project = Project(path_to_file, tasks, R, l)
     return(project)
 
-project = load_instance('psp2.sch')
+test_instances_dir = Path("test_instances")
+project = load_instance(test_instances_dir/'test_ex.sch')
 temporal_analysis(project)
 
 
